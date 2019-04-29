9892d8d8f1551e1e665a9902731dda3c63927152
JshReaper <jshchansen@gmail.com>
2019-04-29 11:29:04 +0200

    added projekt files

---
+++ GlfwTest/Game.h
@@ -0,0 +1,13 @@
+#include "shader_s.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>#pragma once
+void InitializeGlfw();
+void Render(_Notnull_ GLFWwindow* window, _Notnull_ Shader shaderProgram, _Notnull_ unsigned int VAO);
+void Update(_Notnull_ GLFWwindow* window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
